13853da105562463b7c82524b0861aecc489f6b6
[Irina] <[kmiranda.2000@gmail.com]>
2018-10-05 13:49:51 -0400

    completed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;
+
 import java.util.Scanner;
 
 public class Main {
@@ -6,26 +7,45 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
-        Main game = new Main();
-        game.checkContinue();
+        System.out.println("What game do you wish to play? numbers/ copycat");
+        //prompt user to pick the game
+        String gameAnswer = s.nextLine();
+        if (gameAnswer.equalsIgnoreCase("copycat")) {
 
-        String answer = s.nextLine();
-        if (answer.equalsIgnoreCase("yes")){
-            game.checkContinue();
+            while (true) {
+                //call method copyCat to start the game
+                copyCat();
+                System.out.println("Would you like to continue? yes/no");
+                String answer = s.nextLine();
+                if (!answer.equalsIgnoreCase("yes")) {
+                    System.out.println(" Thanks for playing");
+                    break;
+                }
+            }
+        }
+        else if (gameAnswer.equalsIgnoreCase("numbers")) {
+
+            while (true) {
+                //call method gameStep to start numbers game
+                gameStep();
+                System.out.println("Would you like to continue? yes/no");
+                String answer = s.nextLine();
+                if (!answer.equalsIgnoreCase("yes")) {
+                    System.out.println(" Thanks for playing");
+                    break;
+                }
+            }
         }
         else {
-            System.out.println("Thanks for playing!");
+            System.out.println("Try again");
         }
-
-
-
-
-
-
-
-	// write your code here
     }
-    public void checkContinue() {
+
+        // write your code here
+
+    //create a method to process a loop for numbers game
+    public static void gameStep() {
+
         Scanner s = new Scanner(System.in);
         System.out.println("Enter the number");
         int number = s.nextInt();
@@ -39,12 +59,28 @@ public class Main {
                 System.out.println(i);
 
 
-
-
             }
         }
-        System.out.println("Would you like to continue? yes/no");
 
+    }
+    //create a method to process a loop for copycat game
+    public static void copyCat() {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your number");
+        int number = s.nextInt();
+
+        for (int i = number; i > 0; i--) {
+            if (i % 5 == 0 && i % 7 == 0) {
+                System.out.println("CopyCat");
+            } else if (i % 7 == 0) {
+                System.out.println("Cat");
+            } else if (i % 5 == 0) {
+                System.out.println("Copy");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 
 }
